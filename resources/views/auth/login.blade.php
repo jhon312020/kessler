11d5bca2aba9a726782f6ac213e3b9b52b3cd531
Charu Prakash <charumrutha.sherpa@blackpond.co.in>
2021-01-23 20:25:58 +0530

    Login layout changed as per templete

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,47 +4,40 @@
 <main>
   <div class="container">
     <div class="row justify-content-center">
-       <div class="col-lg-7">
+       <div class="col-lg-5">
          <div class="card shadow-lg border-0 rounded-lg mt-5">
             <div class="card-header"><h3 class="text-center font-weight-light my-4">Login</h3>
             </div>
             <div class="card-body">
             <form method="POST" action="{{ route('login') }}">
             @csrf
-            <div class="form-group row">
-            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-            <div class="col-md-6">
-            <input class="form-control @error('email') is-invalid @enderror" name="email" id="email" type="email" value="{{ old('email') }}" required autocomplete="email" autofocus placeholder="Enter email address" />
+            <div class="form-group">
+            <label for="email" class="small mb-1">{{ __('Email') }}</label>
+            <input class="form-control py-4 @error('email') is-invalid @enderror" name="email" id="email" type="email" value="{{ old('email') }}" required autocomplete="email" autofocus placeholder="Enter email address" />
             @error('email')
                 <span class="invalid-feedback" role="alert">
                   <strong>{{ $message }}</strong>
                 </span>
             @enderror
             </div>
-            </div>
-            <div class="form-group row">
-            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-            <div class="col-md-6">
-            <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password" placeholder="Enter password">
+            <div class="form-group">
+            <label for="password" class="small mb-1">{{ __('Password') }}</label>
+            <input id="password" type="password" class="form-control py-4 @error('password') is-invalid @enderror" name="password" required autocomplete="current-password" placeholder="Enter password">
             @error('password')
             <span class="invalid-feedback" role="alert">
                 <strong>{{ $message }}</strong>
             </span>
             @enderror
+           
+            </div>
+            <div class="form-group">
+            <div class="custom-control custom-checkbox">
+             <input class="custom-control-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                <label class="custom-control-label" for="remember">{{ __('Remember Password') }}</label>
             </div>
             </div>
-            <div class="form-group row">
-            <div class="col-md-6 offset-md-4">
-            <div class="form-check">
-             <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-                <label class="form-check-label" for="remember">{{ __('Remember Me') }}</label>
-            </div>
-            </div>
-            </div>
-            <div class="form-group row mb-0">
-              <div class="col-md-8 offset-md-4">
+            <div class="form-group d-flex align-items-center justify-content-between mt-4 mb-0 float-right">
                 <button type="submit" class="btn btn-primary">{{ __('Login') }}</button>
-              </div>
             </div>
         </form>
       </div>
