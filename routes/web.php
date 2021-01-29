3656f00b3859443e0a3fadfc6ee484f63a523dcd
Admin <admin@Admins-MacBook-Pro.local>
2021-01-29 20:59:23 +0530

    Changed web routes as per sessions

--- routes/web.php
+++ routes/web.php
@@ -40,18 +40,26 @@ Route::resource('/story', StoryController::class);
 Route::resource('/words', WordsController::class);
 // ---------------------------------------- ./ ADMIN --------------------------------------------- //
 
-// ---------------------------------------- / SESSIONS /--------------------------------------------- //
+// ---------------------------------------- / SESSIONS PIN /--------------------------------------------- //
 Route::post('/index', [SessionController::class, 'index']);
 Route::get('/index', [SessionController::class, 'index']);
 Route::get('/', [SessionController::class, 'index']);
+// ---------------------------------------- / SESSIONS PIN /--------------------------------------------- //
+
+// ---------------------------------------- / SESSIONS 1-4 /---------------------------------------------- //
 Route::get('/sessions',[SessionController::class, 'sessions']);
+Route::get('/recallwords', [SessionController::class, 'recall']);
+Route::post('/sessions', [SessionController::class,'store']);
+Route::post('/next', [AjaxController::class,'store']);
+Route::get('/complete', [SessionController::class,'complete']);
+// ---------------------------------------- ./ SESSIONS 1-4 / ----------------------------------------- //
+
+// ---------------------------------------- / SESSIONS 5-8 /-------------------------------------------- //
 Route::get('/writings',[SessionController::class, 'writings']);
 Route::post('/story',[SessionController::class, 'writeup']);
 Route::get('/recallwords', [SessionController::class, 'recall']);
-Route::post('/sessions', [SessionController::class,'store']);
 Route::post('/cue', [SessionController::class,'save']);
 Route::get('/cue', [SessionController::class,'save']);
-Route::post('/next', [AjaxController::class,'store']);
 Route::post('/after', [AjaxController::class,'save']);
-Route::get('/complete', [SessionController::class,'complete']);
-// ---------------------------------------- ./ SESSIONS / ----------------------------------------- //
+
+// ---------------------------------------- ./ SESSIONS 5-8 /--------------------------------------------- //
