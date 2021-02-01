0ddd84093fa34c97055ab966479a9d6695c97e59
Janakiraman <janakiraman@proisc.com>
2021-02-01 15:00:25 +0530

    Session 5 to 8 and changes for session type

---
+++ app/Http/Controllers/TypeController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Type;
+
+class TypeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct() {
+      $this->middleware('auth');
+      parent::__construct();
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index() {
+      $type = Type::all();
+      return view('kessler.type.index', compact('type'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create() {
+      return view('kessler.type.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request) {
+      $request->validate([
+        'type'=>'required'
+      ]);
+      
+      $type = new Type([
+        'type' => $request->get('type')
+      ]);
+      $type->save();
+      return redirect('/type')->with('success', 'SESSION TYPE SAVED!');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id) {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id) {
+      $type = Type::find($id);
+      return view('kessler.type.edit', compact('type'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id) {
+      $request->validate([
+        'type'=>'required'
+      ]);
+      $type = Type::find($id);
+      $type->type = $request->get('type');
+      $type->save();
+      return redirect('/type')->with('success', 'SESSION TYPE UPDATED!');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id) {
+      $type = Type::find($id);
+      $type->delete();
+      return redirect('/type')->with('success', 'SESSION TYPE DELETED!');
+    }
+}
---
+++ resources/views/kessler/type/index.blade.php
@@ -0,0 +1,63 @@
+@extends('kessler.layouts.master')
+@section('content')
+  <!-- Content Wrapper. Contains page content -->
+  <div class="container-fluid">
+    <h1 class="mt-4">Session Type</h1>
+    <div class="card mb-4">
+      <div class="card-body">
+        Create, View, Edit and Remove Session Type
+      </div>
+    </div>
+    <div class="card mb-4">
+      <div class="card-header">
+        <i class="fas fa-table mr-1"></i>
+          Session Type
+      </div>
+      <br/>
+      <a href="{{ route('type.create') }}" class="btn btn-primary btn-block bg-gradient-primary" style="width: fit-content; margin-left: 25px;">Add Session Type</a>
+      <div class="card-body">
+        <div class="table-responsive">
+          <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+            <thead>
+              <tr>
+                <th>S.No</th>
+                <th>Session Type</th>
+                <th width="20%">Actions</th>
+              </tr>
+            </thead>
+            <tfoot>
+              <tr>
+                <th>S.No</th>
+                <th>Session Type</th>
+                <th width="20%">Actions</th>
+              </tr>
+            </tfoot>
+            <tbody>
+            @foreach($type as $type)
+              <tr>
+                <td>{{$type->id}}</td>
+                <td>{{$type->type}}</td>
+                <td>
+                  <a href="{{ route('type.edit', $type->id)}}" class="btn btn-primary" role="button"><i class="fa fa-edit">&nbsp;</i>Edit</a>
+                  <form action="{{ route('type.destroy', $type->id)}}" method="post" class="d-inline">
+                    @csrf
+                    @method('DELETE')
+                    <button class="btn btn-danger" type="submit"><i class="fa fa-trash">&nbsp;</i>Delete</button>
+                  </form>
+                </td>
+              </tr>
+            @endforeach
+            </tbody>
+          </table>
+          <div>
+          @if(session()->get('success'))
+            <div class="alert alert-success">
+              {{ session()->get('success') }}  
+            </div>
+          @endif
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+@endsection
