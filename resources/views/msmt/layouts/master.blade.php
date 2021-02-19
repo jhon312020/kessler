f1295cc7bbef8e38ce3589e2b047178533bf7e04
Janakiraman <janakiraman@proisc.com>
2021-02-19 20:49:41 +0530

    Fixed the issue that was found in testing.

--- resources/views/msmt/layouts/master.blade.php
+++ resources/views/msmt/layouts/master.blade.php
@@ -1,43 +1,53 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
-    <title>Kessler Foundation</title>
+    <link rel="icon" href="/docs/4.0/assets/img/favicons/favicon.ico">
+
+    <title>Starter Template for Bootstrap</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/starter-template/">
+
     <!-- Bootstrap core CSS -->
     <link href="{{asset('vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
     <link href="{{asset('css/app.css')}}" rel="stylesheet">
     <script src="{{asset('vendor/jquery/jquery.min.js')}}"></script>
   </head>
   @isset($page)
-  <body class="grey-background">
+    <body class="grey-background">
   @else
-  <body class="white-background">
+    <body class="white-background">
   @endisset
-    <!-- Page Content -->
-    <div id="container" class="container ">
+    <main role="main" class="container">
       <div id="header">
         <img src="{{asset('assets/img/logo.png')}}" alt="logo" />
       </div>
+      <div class="starter-template">
         @yield('content')
-      <div id="footer">
-        @isset($page)
-          <img src="{{asset('assets/img/footer_grey.png')}}" class="center"/>
-        @else 
-          <img src="{{asset('assets/img/footer_white.png')}}" class="center"/>
-        @endisset
       </div>
+
+    </main><!-- /.container -->
+    <div id="footer">
+      @isset($page)
+        <img src="{{asset('assets/img/footer_grey.png')}}" class="center"/>
+      @else 
+        <img src="{{asset('assets/img/footer_white.png')}}" class="center"/>
+      @endisset
     </div>
-    <!-- Bootstrap core JavaScript -->
-    
-    <script src="{{asset('vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="{{asset('vendor/bootstrap/js/bootstrap.min.js')}}"></script>
     <script src="{{asset('js/confetti.min.js')}}"></script>
     <script type='text/javascript'>
       if ( window.history.replaceState ) {
           window.history.replaceState( null, null, window.location.href );
       }
-  </script>
+    </script>
   </body>
 </html>
