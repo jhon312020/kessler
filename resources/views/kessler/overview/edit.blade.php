20be5f4d5a166506419e4456b929eb41dc544cb8
Admin <admin@Admins-MacBook-Pro.local>
2021-02-24 12:20:13 +0530

    Overview icons added

--- resources/views/kessler/overview/edit.blade.php
+++ resources/views/kessler/overview/edit.blade.php
@@ -20,11 +20,11 @@
               @csrf
               <div class="form-group">
                 <label class="small mb-1" for="overview">Update Overview</label>
-                <textarea class="form-control py-4" name="overview"  style="height: 218px;" rows="30" cols="150" placeholder="Enter ..." autofocus>{{$overview->overview}}</textarea>
+                <textarea class="form-control py-4" name="overview"  style="height: 218px;" rows="30" cols="150" placeholder="Enter overview ..." autofocus>{{$overview->overview}}</textarea>
               </div>
               <div class="form-group d-flex align-items-center float-right mt-4 mb-0">
-                <button type="submit" class="btn btn-primary">Update</button>
-                <a href="{{ url('/overview')}}" class="ml-2 btn btn-danger" role="button">Cancel</a>
+                <button type="submit" class="btn btn-primary"><i class="fas fa-sync">&nbsp;</i> Update</button>
+                <a href="{{ url('/overview')}}" class="ml-2 btn btn-danger" role="button"><i class="fas fa-times">&nbsp;</i> Cancel</a>
               </div>
             </form>
           </div>
