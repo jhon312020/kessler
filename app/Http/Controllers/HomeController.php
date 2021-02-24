a1d86c3d7e26dbbe075000ed0cf7f372993b12c8
Admin <admin@Admins-MacBook-Pro.local>
2021-02-24 19:20:23 +0530

    Admin Dashboard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Auth;
+use App\Models\User;
+use App\Models\Trainee;
 
 class HomeController extends Controller
 {
@@ -20,10 +22,26 @@ class HomeController extends Controller
     /**
      * Show the application dashboard.
      *
+     * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function index() {
-      return view('kessler.admin.dashboard');
+    public function index(Request $request) {
+      $kessler = User::where('role', 'SA')->get();
+      //$this->pr($kessler->toArray()); //exit();
+      $user = Auth::user();
+      $trainerID = Trainee::select('trainer_id')->where('trainer_id', $user->id)->get();
+      //$this->pr($trainerID->toArray()); exit();
+      $trainer = User::where('role', 'TA')->get();
+      //$this->pr($trainer->toArray()); exit();
+      $trainerCount = $trainer->count();
+      $trainee = Trainee::where('trainer_id', $user->id)->get();
+      //$this->pr($trainee->toArray()); exit();
+      $traineeCount = $trainee->count();
+      $traineeInProgress = Trainee::where('completed', 0)->where('trainer_id', $user->id)->get();
+      $traineeInProgressCount = $traineeInProgress->count();
+      $traineeCompleted = Trainee::where('completed', 1)->where('trainer_id', $user->id)->get();
+      $traineeCompletedCount = $traineeCompleted->count();
+      return view('kessler.admin.dashboard',compact('kessler','trainer','trainerCount','trainee','traineeCount', 'traineeInProgressCount', 'traineeCompletedCount'));
     }
 
     public function logout() {
--- resources/views/kessler/admin/dashboard.blade.php
+++ resources/views/kessler/admin/dashboard.blade.php
@@ -8,15 +8,33 @@
     </ol>
     <div class="row">
         <div class="col-xl-3 col-md-6">
-            <div class="card bg-primary text-white mb-4">
-                <div class="card-body">Primary Card</div>
+            <div class="card bg-success text-white mb-4">
+                @if($traineeCount)
+                <div class="card-body">Trainee :&emsp;{{$traineeCount}} 
+                  <br> In Progress :&emsp;{{$traineeInProgressCount}} <br> Completed :&emsp;{{$traineeCompletedCount}}
+                </div>
+                @endif
                 <div class="card-footer d-flex align-items-center justify-content-between">
-                    <a class="small text-white stretched-link" href="#">View Details</a>
+                    <a class="small text-white stretched-link" href="{{ url('/trainee')}}">View Details</a>
                     <div class="small text-white"><i class="fas fa-angle-right"></i></div>
                 </div>
             </div>
         </div>
+        @if($kessler)  
         <div class="col-xl-3 col-md-6">
+            <div class="card bg-primary text-white mb-4">
+                @if($trainerCount)
+                <div class="card-body">Trainer :&emsp;{{$trainerCount}}</div>
+                <br><br>
+                @endif
+                <div class="card-footer d-flex align-items-center justify-content-between">
+                    <a class="small text-white stretched-link" href="{{ url('/trainer')}}">View Details</a>
+                    <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+                </div>
+            </div>
+        </div>
+        @endif
+       <!--  <div class="col-xl-3 col-md-6">
             <div class="card bg-warning text-white mb-4">
                 <div class="card-body">Warning Card</div>
                 <div class="card-footer d-flex align-items-center justify-content-between">
@@ -24,8 +42,8 @@
                     <div class="small text-white"><i class="fas fa-angle-right"></i></div>
                 </div>
             </div>
-        </div>
-        <div class="col-xl-3 col-md-6">
+        </div> -->
+<!--         <div class="col-xl-3 col-md-6">
             <div class="card bg-success text-white mb-4">
                 <div class="card-body">Success Card</div>
                 <div class="card-footer d-flex align-items-center justify-content-between">
@@ -33,8 +51,8 @@
                     <div class="small text-white"><i class="fas fa-angle-right"></i></div>
                 </div>
             </div>
-        </div>
-        <div class="col-xl-3 col-md-6">
+        </div> -->
+<!--         <div class="col-xl-3 col-md-6">
             <div class="card bg-danger text-white mb-4">
                 <div class="card-body">Danger Card</div>
                 <div class="card-footer d-flex align-items-center justify-content-between">
@@ -42,7 +60,7 @@
                     <div class="small text-white"><i class="fas fa-angle-right"></i></div>
                 </div>
             </div>
-        </div>
+        </div> -->
     </div>
 </div>
 @endsection
