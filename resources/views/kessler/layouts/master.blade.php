0db8414037d5748943aabec878df12b0b4bee10e
Admin <admin@Admins-MacBook-Pro.local>
2021-02-25 15:21:22 +0530

    Graph report

--- resources/views/kessler/layouts/master.blade.php
+++ resources/views/kessler/layouts/master.blade.php
@@ -98,8 +98,9 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
     <script src="{{asset('assets/demo/chart-area-demo.js')}}"></script>
     <script src="{{asset('assets/demo/chart-bar-demo.js')}}"></script>
+    <script src="{{asset('assets/demo/chart-pie-demo.js')}}"></script>
     <script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js" crossorigin="anonymous"></script>
     <script src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js" crossorigin="anonymous"></script>
-    <script src="{{asset('assets/demo/datatables-demo.js')}}"></script>
+    <script src="{{asset('assets/demo/datatables-demo.js')}}"></script>        
   </body>
 </html>
