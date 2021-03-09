daa0ece8e10ebf701e53d1d453bd3f4e97c61eb1
Admin <admin@Admins-MacBook-Pro.local>
2021-03-09 20:12:07 +0530

    404 Unauthorized access layout

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -4,15 +4,17 @@
     <div id="layoutError_content">
         <main>
             <div class="container">
-                <div class="row justify-content-center">
-                    <div class="col-lg-6">
-                        <div class="text-center mt-4">
-                            <img class="mb-4 img-error" src="{{asset('assets/img/error-404-monochrome.svg')}}" />
-                            <p class="lead">This requested URL was not found on this server.</p>
-                            <a href="{{ url('/dashboard') }}">
-                                <i class="fas fa-arrow-left mr-1"></i>
-                                Return to Dashboard
-                            </a>
+                    <div class="row justify-content-center">
+                        <div class="col-lg-6">
+                            <div class="text-center mt-4">
+                                <h1 class="display-1">401</h1>
+                                <p class="lead">Unauthorized</p>
+                                <p>Access to this resource is denied.</p>
+                                <a href="{{url('dashboard')}}">
+                                    <i class="fas fa-arrow-left mr-1"></i>
+                                    Return to Dashboard
+                                </a>
+                            </div>
                         </div>
                     </div>
                 </div>
