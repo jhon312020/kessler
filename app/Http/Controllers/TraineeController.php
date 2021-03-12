36ff160bfbd6bf623268631e3824fa2598a75dd7
Admin <admin@Admins-MacBook-Pro.local>
2021-03-12 19:52:00 +0530

    Fixed bug for pagination

--- app/Http/Controllers/TraineeController.php
+++ app/Http/Controllers/TraineeController.php
@@ -112,13 +112,12 @@ class TraineeController extends Controller
         // Total records
         $totalRecords = Trainee::select('count(*) as allcount')->count();
         // $this->pr($totalRecords);
-        //$totalRecordswithFilter = Trainee::select('count(*) as allcount')->where('trainee_id', 'like', '%' .$searchValue . '%')->count();
+        $totalRecordswithFilter = Trainee::select('count(*) as allcount')->where('trainee_id', 'like', '%' .$searchValue . '%')->count();
         //$this->pr($totalRecordswithFilter);
 
         // Fetch records
-        $trainees = Trainee::select('*')->skip($start)->take($rowperpage)->get();
+        $trainees = Trainee::select('trainees.*')->skip($start)->take($rowperpage)->get();
         //$this->pr($trainees->toArray());
-
         $data_arr =  array();
         $sno = $start+1;
         foreach ($trainees as $records) {
@@ -133,11 +132,11 @@ class TraineeController extends Controller
             if ($sessionStartTime) {
                 $session_start_time =  date('m/d/Y h:i a', strtotime($sessionStartTime->roundOne));
             }
-              if ($sessionEndTime && $records->round === 1) {
+            if ($sessionEndTime && $records->round === 1) {
                 $session_end_time =  date('m/d/Y h:i a', strtotime($sessionEndTime->roundOne));
-              } 
-              if ($sessionEndTime && $records->completed === 1) {
-              $session_end_time =  date('m/d/Y h:i a', strtotime($sessionEndTime->roundTwo));
+            } 
+            if ($sessionEndTime && $records->completed === 1) {
+                $session_end_time =  date('m/d/Y h:i a', strtotime($sessionEndTime->roundTwo));
             }
           $session_start_time = $session_start_time;
           $session_end_time = $session_end_time;
@@ -148,6 +147,7 @@ class TraineeController extends Controller
           $delete = route('trainee.destroy', $records->id);
           $report = url('trainee/report', $records->id);
           $approve = url('trainee/approve', $records->id);
+          $id = $records->id;
           $action =  "<a href='$add' class='btn btn-primary' role='button' title='Add'><i class='fas fa-plus' title='Add'></i></a>&nbsp;";
            $action .= "<a href='$view' class='btn btn-primary' role='button' title='View'><i class='fas fa-eye' title='View'></i></a>&nbsp;";
            if ($records->session_number > 4 && $records->session_type == "A") {
@@ -158,8 +158,8 @@ class TraineeController extends Controller
           }
           if ($records->completed == 0) {
             $action .= "<a href='$edit' class='btn btn-primary' role='button' title='Edit'><i class='fas fa-edit' title='Edit'></i></a>&nbsp;";
-            $action .= "<form action='$delete' method='post' class='d-inline' id='jsSubmitForm-{{ $records->id }}'>
-                    <button class='btn btn-danger jsConfirmButton' type='button' data-value='{{ $records->id }}' title='Delete'><i class='fa fa-trash' title='Delete'></i> </button></form>";  
+            $action .= "<form action='$delete' method='post' class='d-inline' id='jsSubmitForm-$id'>
+                      <button class='btn btn-danger jsConfirmButton' type='button' data-value='$id' title='Delete'><i class='fa fa-trash' title='Delete'></i></button></form>";  
           }
           if ($records->completed == 1) {
             $action .= "<a href='$report' class='btn btn-primary' role='button' title='Report'><i class='fas fa-chart-pie' title='Report'></i></a>&nbsp;";
@@ -177,12 +177,9 @@ class TraineeController extends Controller
           );
         }
           $response = array(
-            // "draw" => intval($draw),
-            // "iTotalRecords" => $totalRecords,
-            // "data" => $data_arr
             "draw" => intval($draw),
             "iTotalRecords" => $totalRecords,
-            //"iTotalDisplayRecords" => $totalRecordswithFilter,
+            "iTotalDisplayRecords" => $totalRecordswithFilter,
             "aaData" => $data_arr
           );
           echo json_encode($response);
